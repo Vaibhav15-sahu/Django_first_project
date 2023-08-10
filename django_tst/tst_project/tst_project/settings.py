58e6e20e962acd823ce01afedba04efaf0b700c1
Vaibhav15-sahu <vaibhavsahu0115@gmail.com>
2023-08-10 15:52:17 +0530

    Creating models

--- django_tst/tst_project/tst_project/settings.py
+++ django_tst/tst_project/tst_project/settings.py
@@ -85,13 +85,18 @@ WSGI_APPLICATION = 'tst_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql', 
+    #     'NAME': 'tst_project',
+    #     'USER': os.environ.get("MYSQL_USER"),
+    #     'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
+    #     'HOST': os.environ.get("MYSQL_HOST"),
+    #     'PORT': '3306',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'tst_project',
-        'USER': os.environ.get("MYSQL_USER"),
-        'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
-        'HOST': os.environ.get("MYSQL_HOST"),
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
